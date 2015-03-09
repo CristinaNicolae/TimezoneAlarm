3252b419ed9b4f0a008c81fd7ab21331046e750b
CristinaN <cristina.nicolae@cst-us.com>
2015-03-09 17:27:27 +0200

    Replaced Toast message with notifications (Example).

--- app/src/main/java/com/cristina/timezonealarm/MainActivity.java
+++ app/src/main/java/com/cristina/timezonealarm/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cristina.timezonealarm;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
@@ -32,6 +34,7 @@ public class MainActivity extends ActionBarActivity {
                 + (i * 1000), pendingIntent);
         Toast.makeText(this, "Alarm set in " + i + " seconds",
                 Toast.LENGTH_LONG).show();
+
     }
 
 
--- app/src/main/java/com/cristina/timezonealarm/MyBroadcastReceiver.java
+++ app/src/main/java/com/cristina/timezonealarm/MyBroadcastReceiver.java
@@ -3,6 +3,9 @@ package com.cristina.timezonealarm;
 /**
  * Created by Cristina on 3/8/2015.
  */
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -18,6 +21,19 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         Vibrator vibrator = (Vibrator) context
                 .getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(2000);
+
+        NotificationManager mNM;
+        mNM = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
+        // Set the icon, scrolling text and timestamp
+        Notification notification = new Notification(R.drawable.plus, "Test Alarm",
+                System.currentTimeMillis());
+        // The PendingIntent to launch our activity if the user selects this notification
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+        // Set the info for the views that show in the notification panel.
+        notification.setLatestEventInfo(context, context.getText(R.string.abc_action_bar_home_description), "This is a Test Alarm", contentIntent);
+        // Send the notification.
+        // We use a layout id because it is a unique number. We use it later to cancel.
+        mNM.notify(R.string.abc_action_bar_home_description, notification);
     }
 
 }
